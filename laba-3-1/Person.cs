525baf49597e4d221a673f9e2f83d78307b783f5
IK-24KudinaDiana <kudinadiana91@gmail.com>
2023-03-31 13:14:12 +0300

    init

---
+++ laba-3-1/Person.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+using System.Xml.Linq;
+
+namespace laba_3_1
+{
+	public class Person
+	{
+		private string _name ;
+
+		public string Name
+		{
+			get { return _name; }
+			set
+			{
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ArgumentNullException("Name no can be null");
+				}
+				_name = value;
+			}
+		}
+		
+	}
+}
---
+++ laba-3-1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using laba_3_1;
+
+Console.WriteLine("Hello, World!");
+
+Person p = new Person();
+p.Name = "Mary";
+Console.WriteLine(p.Name);
+Console.ReadKey();
